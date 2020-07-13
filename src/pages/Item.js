e44b08f62749a33d3c26e5196082bf0cacca279d
Jerry <jchen123.862@gmail.com>
2020-07-13 21:18:02 +1200

    populate display and item page with data from api

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,93 +1,107 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 import img from '../assets/icons/primary.jpg';
 
 class Item extends React.Component {
+    state = {
+        item: {},
+        loaded: false
+    };
+
+    componentDidMount() {
+        const itemId = window.location.pathname.split('/')[2];
+        axios.get('http://localhost:4001/api/items/' + itemId).then(res => {
+            this.setState({item:res.data, loaded: true});
+        });
+    };
+
     render () {
-        return (
-            <div className='item'>
-                <div className='item-top'>
-                    <div className='item-left'>
-                        <div className='items-container'>
-                            <img style={{padding: '0 10px 10px 0'}} src={img}/>
-                            <img style={{padding: '0 0 10px 10px'}} src={img}/>
+        if (this.state.loaded === true) {
+            let images = this.state.item.images.map((img, index) => {
+                return <img key={index} style={{padding: '0 10px 10px 0'}} src={img}/>
+            });
+
+            let size = this.state.item.sizes.map((size, index) => {
+                return <option key={index}>{size}</option>
+            });
+
+            let color = this.state.item.colors.map((color, index) => {
+                return <option key={index}>{color}</option>
+            });
+
+            return (
+                <div className='item'>
+                    <div className='item-top'>
+                        <div className='item-left'>
+                            {images}
                         </div>
-                        <div className='items-container'>
-                            <img style={{padding: '10px 10px 10px 0'}} src={img}/>
-                            <img style={{padding: '10px 0 10px 10px'}} src={img}/>
-                        </div>
-                        <div className='items-container'>
-                            <img style={{padding: '10px 10px 0 0'}} src={img}/>
-                            <img style={{padding: '10px 0 10px 10px'}} src={img}/>
+                        <div className='item-right'>
+                            <div className='item-info'>
+                                <p className='item-type'>{this.state.item.type}</p>
+                                <p className='item-price'>{'$' + this.state.item.price}</p>
+                            </div>
+                            <h1 className='item-name'>{this.state.item.name}</h1>
+                            <p className='item-select-info'>SIZE</p>
+                            <select name='size'>
+                                {size}
+                            </select>
+                            <p className='item-select-info'>COLOR</p>
+                            <select name='color'>
+                                {color}
+                            </select>
+                            <button className='add'>ADD</button>
+                            <button className='favorite'>FAVORITE</button>
                         </div>
                     </div>
-                    <div className='item-right'>
-                        <div className='item-info'>
-                            <p className='item-type'>TEE</p>
-                            <p className='item-price'>$150</p>
+                    <div className='item-bottom'>
+                        <h1>YOU MIGHT ALSO LIKE</h1>
+                        <div className='item-options-container'>
+                            <div className='item-option'>
+                                <div className='item-option-inner'>
+                                    <Link to='/item'>
+                                        <img src={img}/>
+                                        <div className='input-align'>
+                                            <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
+                                            <p style={{marginLeft: 'auto'}}>$150</p>
+                                        </div>
+                                        <p>SHIRT</p>
+                                    </Link>
+                                </div>
+                            </div>
+    
+                            <div className='item-option'>
+                                <div className='item-option-inner'>
+                                    <Link to='/item'>
+                                        <img src={img}/>
+                                        <div className='input-align'>
+                                            <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
+                                            <p style={{marginLeft: 'auto'}}>$150</p>
+                                        </div>
+                                        <p>SHIRT</p>
+                                    </Link>
+                                </div>
+                            </div>
+    
+                            <div className='item-option'>
+                                <div className='item-option-inner'>
+                                    <Link to='/item'>
+                                        <img src={img}/>
+                                        <div className='input-align'>
+                                            <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
+                                            <p style={{marginLeft: 'auto'}}>$150</p>
+                                        </div>
+                                        <p>SHIRT</p>
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                        <h1 className='item-name'>TOM AND JERRY</h1>
-                        <p className='item-select-info'>SIZE</p>
-                        <select>
-                            <option>S</option>
-                            <option>M</option>
-                            <option>L</option>
-                        </select>
-                        <p className='item-select-info'>COLOR</p>
-                        <select>
-                            <option>S</option>
-                            <option>M</option>
-                            <option>L</option>
-                        </select>
-                        <button className='add'>ADD</button>
-                        <button className='favorite'>FAVORITE</button>
                     </div>
                 </div>
-                <div className='item-bottom'>
-                    <h1>YOU MIGHT ALSO LIKE</h1>
-                    <div className='item-options-container'>
-                        <div className='item-option'>
-                            <div className='item-option-inner'>
-                                <Link to='/item'>
-                                    <img src={img}/>
-                                    <div className='input-align'>
-                                        <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
-                                        <p style={{marginLeft: 'auto'}}>$150</p>
-                                    </div>
-                                    <p>SHIRT</p>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className='item-option'>
-                            <div className='item-option-inner'>
-                                <Link to='/item'>
-                                    <img src={img}/>
-                                    <div className='input-align'>
-                                        <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
-                                        <p style={{marginLeft: 'auto'}}>$150</p>
-                                    </div>
-                                    <p>SHIRT</p>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className='item-option'>
-                            <div className='item-option-inner'>
-                                <Link to='/item'>
-                                    <img src={img}/>
-                                    <div className='input-align'>
-                                        <p style={{fontWeight: 'bold'}}>TOM AND JERRY</p>
-                                        <p style={{marginLeft: 'auto'}}>$150</p>
-                                    </div>
-                                    <p>SHIRT</p>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+            )
+        } else {
+            return <div></div>
+        }
     }
 }
 
